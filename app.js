726875bbe3ea76fbe1d8b90058a2fc00086e06d9
Grzegorz Kaczor <grzesiek125@op.pl>
2022-07-25 10:31:09 +0200

    get & post

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Conrtoll-Allow-Origin', '*');
+    res.setHeader('Access-Conrtoll-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Conrtoll-Allow-Headers', 'Content-Type', 'Authorization');
+    next();
+});
 
 app.use('/feed', feedRoutes);
 
