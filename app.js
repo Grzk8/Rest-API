a9f24e3a04a5fb6b215eab5958eb0ab992600c74
Grzegorz Kaczor <grzesiek125@op.pl>
2022-07-28 19:43:53 +0200

    backend finished

--- app.js
+++ app.js
@@ -56,7 +56,11 @@ app.use((error, req, res, next) => {
 mongoose.connect(
     'mongodb+srv://user1:user1haslo@cluster0.wcshf.mongodb.net/messages?retryWrites=true'
     ).then(result => {
-        app.listen(8080);
+        const server = app.listen(8080);
+        const io = require('./socket').init(server);
+        io.on('connection', socket => {
+          console.log('Client connected');
+        });
     })
     .catch(err => {
         console.log(err);
